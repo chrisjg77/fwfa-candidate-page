4e84a2376b0abe4bd141dec46e7b034153c531f3
Nick Badagliacca <nick@terraeclipse.com>
2013-12-23 16:50:23 -0800

    First pages commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function() {
+
+  // FUNCTIONS
+  // ======================================================
+  
+  function setContentOffset(navSelector, contentSelector) {
+    var offsetRef   = $(navSelector)
+        , offsetObj = $(contentSelector)
+        , refHeight = offsetRef.outerHeight();
+
+    offsetObj.css({ marginTop: refHeight + 'px' });
+  }
+
+  function switchTab(tabsSelector, clickedTabId) {
+    var activeTab           = $(tabsSelector).find('li.active')
+        , activeTabContent  = $('.tab-content.active')
+        , clickedTab        = $('#' + clickedTabId)
+        , clickedTabContent = $('#' + clickedTabId + '-content');
+
+    activeTab.removeClass('active');
+    activeTabContent.removeClass('active');
+
+    clickedTab.addClass('active');
+    clickedTabContent.addClass('active');
+  }
+
+
+  // DOCUMENT READY
+  // ======================================================
+
+  $(document).ready(function() {
+    setContentOffset('.nav-bar', '.wrapper');
+  });
+
+
+  // WINDOW RESIZE
+  // ======================================================
+
+  $(window).resize(function() {
+    setContentOffset('.nav-bar', '.wrapper');
+  });
+
+
+  // TAB CONTROLS
+  // ======================================================
+
+  $('.tabs li').on('click', function() {
+    var tabsSelector = $(this).parents('.tabs')
+        , clickedTabId = $(this).attr('id');
+
+    switchTab(tabsSelector, clickedTabId);
+  });
+
+});
